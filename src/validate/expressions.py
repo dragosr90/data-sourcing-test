bd757110f2a419d4f5c5f04331781547c91e7231
dragosr90 <dragos.raduta@yahoo.com>
2025-05-23 14:37:28 +0300

    Update expressions.py

--- src/validate/expressions.py
+++ src/validate/expressions.py
@@ -1,9 +1,10 @@
 from pyspark.sql import SparkSession
 
+from src.config.business_logic import SourceConfig
 from src.utils.logging_util import get_logger
 from src.utils.sources_util import (
     keep_unique_sources,
-    update_join_sources_expressions,
+    update_join_sources_transformations,
     update_sources,
     update_variables,
 )
@@ -20,44 +21,52 @@ class Expressions(BaseValidate):
         business_logic: dict,
     ) -> None:
         super().__init__(spark, business_logic)
-        self.sources = self.business_logic["sources"]
+        self.sources: list[SourceConfig] = self.business_logic["sources"]
         self.expressions = self.business_logic["expressions"]
         self.transformations = self.business_logic.get("transformations")
 
     def validate(self) -> bool:
         """Validate expressions from business logic mapping."""
-        updated_sources = self.sources
-        updated_variables: list[str] = []
+        available_variables: list[str] = []
+        updated_sources: list[SourceConfig] = self.sources
 
         if self.transformations:
             for tf in self.transformations:
                 # Take first key of the dictionary, the transformation step name
+                # transformation mapping should not be empty
                 tf_step = next(iter(tf))
                 if tf_step == "join":
                     left_source, right_source = (
                         tf.get("join").get(f"{s}_source") for s in ["left", "right"]
                     )
-
-                    updated_sources = update_join_sources_expressions(
-                        updated_sources, self.sources, left_source, right_source
+                    updated_sources = update_join_sources_transformations(
+                        self.sources, updated_sources, left_source, right_source
                     )
+                    available_variables = update_variables(available_variables, tf=tf)
 
-                elif tf_step in ["aggregation", "pivot", "union"]:
-                    updated_sources = update_sources(
-                        input_sources=updated_sources, tf=tf, tf_step=tf_step
-                    )
-                    # Update 'data dictionary' with new aliases
-                    self.sources = keep_unique_sources(updated_sources, self.sources)
+                # Update available variables and sources after last transformation step
+                updated_sources = update_sources(
+                    input_sources=updated_sources, tf=tf, tf_step=tf_step
+                )
+                # Update 'data dictionary' with new aliases
+                self.sources = keep_unique_sources(updated_sources, self.sources)
 
                 # If left source is specified, drop all updated variables
                 # If tf_step in aggregation, pivot, union, drop all variables
-                updated_variables = update_variables(updated_variables, tf=tf)
+                available_variables = update_variables(available_variables, tf=tf)
+
+            if tf[tf_step].get("alias"):
+                updated_sources = [
+                    source
+                    for source in updated_sources
+                    if source["alias"] == tf[tf_step].get("alias")
+                ]
 
         if not validate_sql_expressions(
             self.spark,
             updated_sources,
             self.expressions,
-            updated_variables if updated_variables else None,
+            available_variables if available_variables else None,
         ):
             return False
         logger.info("Target expressions validated successfully")
