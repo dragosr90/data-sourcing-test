78ac48a445d815c7c9b311da3486b96b7d348cca
dragosr90 <dragos.raduta@yahoo.com>
2025-06-17 16:33:13 +0300

    Create test_extract_non_ssf_data.py

---
+++ test/staging/test_extract_non_ssf_data.py
@@ -0,0 +1,272 @@
+import re
+from datetime import datetime, timezone
+from pathlib import Path
+from unittest.mock import call
+
+import pytest
+from pyspark.sql.types import IntegerType, StringType, StructField, StructType
+
+from src.staging.extract_nonssf_data import ExtractNonSSFData
+
+
+class FileInfoMock(dict):
+    """Allows to access dictionary keys as attributes.
+    And adds a simple isDir method."""
+
+    __getattr__ = dict.get
+
+    def isDir(self):  # noqa: N802
+        return bool(self.name.endswith("/") or self.name.endswith(".parquet"))
+
+
+@pytest.mark.parametrize(
+    ("run_month", "source_container"),
+    [("202503", "test-container")],
+)
+def test_extract_non_ssf_data(
+    spark_session,
+    mocker,
+    run_month,
+    source_container,
+    caplog,
+):
+    test_container = f"abfss://{source_container}@bsrcdadls.dfs.core.windows.net"
+    month_container = f"abfss://{run_month}@bsrcdadls.dfs.core.windows.net"
+    metadata_path = f"bsrc_d.metadata_{run_month}.metadata_nonssf"
+    log_path = f"bsrc_d.log_{run_month}.log_nonssf"
+
+    # Create a mock DataFrame
+    schema_meta = [
+        "SourceSystem",
+        "SourceFileName",
+        "SourceFileFormat",
+        "SourceFileDelimiter",
+        "StgTableName",
+        "FileDeliveryStep",
+        "FileDeliveryStatus",
+    ]
+    mock_meta = spark_session.createDataFrame(
+        [
+            (
+                "lrd_static",
+                "TEST_NON_SSF_V1",
+                ".txt",
+                "|",
+                "test_non_ssf_v1",
+                0,
+                "Expected",
+            ),
+            (
+                "lrd_static",
+                "TEST_NON_SSF_V2",
+                ".txt",
+                "|",
+                "test_non_ssf_v2",
+                0,
+                "Expected",
+            ),
+            (
+                "nme",
+                "TEST_NON_SSF_V3",
+                ".parquet",
+                ",",
+                "test_non_ssf_v3",
+                0,
+                "Expected",
+            ),
+            (
+                "finob",
+                "TEST_NON_SSF_V4",
+                ".csv",
+                ",",
+                "test_non_ssf_v4",
+                0,
+                "Expected",
+            ),
+        ],
+        schema=schema_meta,
+    )
+
+    schema_log = [
+        "SourceSystem",
+        "SourceFileName",
+        "DeliveryNumber",
+        "FileDeliveryStep",
+        "FileDeliveryStatus",
+        "Result",
+        "LastUpdatedDateTimestamp",
+        "Comment",
+    ]
+    mock_log = spark_session.createDataFrame(
+        [
+            (
+                "lrd_static",
+                "TEST_NON_SSF_V1",
+                1,
+                0,
+                "Expected",
+                "Success",
+                datetime.now(timezone.utc),
+                "Test comment",
+            )
+        ],
+        schema=schema_log,
+    )
+
+    dummy_df = spark_session.createDataFrame(
+        [(1, "2", 3)],
+        schema=StructType(
+            [
+                StructField("col1", IntegerType()),
+                StructField("col2", StringType()),
+                StructField("col3", IntegerType()),
+            ]
+        ),
+    )
+
+    # Mock spark.read.json and spark.read.table to return the mock DataFrames
+    mock_read = mocker.patch("pyspark.sql.SparkSession.read", autospec=True)
+    mock_read.table.side_effect = [
+        mock_meta,
+        mock_log,
+        dummy_df,
+        dummy_df,
+        dummy_df,
+        dummy_df,
+    ]
+
+    mock_write = mocker.patch("pyspark.sql.DataFrameWriter.parquet")
+    mock_save_table = mocker.patch("pyspark.sql.DataFrameWriter.saveAsTable")
+
+    # Check ExtractNonSSFData class initialisation
+    extraction = ExtractNonSSFData(
+        spark_session,
+        run_month,
+        source_container=source_container,
+    )
+
+    # Verify that spark.read.table was called with the correct arguments
+    mock_read.table.assert_any_call(f"bsrc_d.metadata_{run_month}.metadata_nonssf")
+    mock_read.table.assert_any_call(f"bsrc_d.log_{run_month}.log_nonssf")
+
+    mock_dbutils_fs_ls = mocker.patch.object(extraction.dbutils.fs, "ls")
+    effect = [
+        [
+            FileInfoMock(
+                {
+                    "path": f"{test_container}/{folder}/{file}",
+                    "name": f"{file}",
+                }
+            )
+            for file, folder in li
+        ]
+        for li in [
+            [
+                ("TEST_NON_SSF_V3.parquet", "NME"),
+                ("TEST_NON_SSF_V3.csv", "NME"),
+                ("processed/", "NME"),
+            ],
+            [("TEST_NON_SSF_V4.csv", "FINOB"), ("processed/", "FINOB")],
+            [
+                ("TEST_NON_SSF_V1.txt", "LRD_STATIC"),
+                ("TEST_NON_SSF_V5.txt", "LRD_STATIC"),
+                ("processed/", "LRD_STATIC"),
+            ],
+            [("TEST_NON_SSF_V2_999999.txt", "LRD_STATIC/processed")],
+            [("TEST_NON_SSF_V2_999999.txt", "LRD_STATIC/processed")],
+        ]
+    ]
+    mock_dbutils_fs_ls.side_effect = effect
+    mock_dbutils_fs_cp = mocker.patch.object(extraction.dbutils.fs, "cp")
+    mock_dbutils_fs_mv = mocker.patch.object(extraction.dbutils.fs, "mv")
+
+    found_files = extraction.get_all_files()
+    mock_dbutils_fs_cp.assert_any_call(
+        f"{test_container}/LRD_STATIC/processed/TEST_NON_SSF_V2_999999.txt",
+        f"{test_container}/LRD_STATIC/TEST_NON_SSF_V2.txt",
+    )
+
+    assert (
+        "File TEST_NON_SSF_V5 not found in metadata. "
+        "Please check if it should be delivered." in caplog.messages
+    )
+
+    mock_read.csv.return_value = dummy_df
+    mock_read.parquet.return_value = dummy_df
+
+    for file in found_files:
+        file_name = file["file_name"]
+        file_name_base = Path(file_name).name
+        file_name_no_ext = Path(file_name).stem
+        file_name_ext = Path(file_name).suffix
+        source_system = file["source_system"]
+
+        result = file_name_base != "TEST_NON_SSF_V3.csv"
+        assert extraction.initial_checks(**file) is result
+
+        if file_name_base == "TEST_NON_SSF_V3.csv":
+            continue
+
+        assert extraction.convert_to_parquet(**file)
+
+        if source_system == "LRD_STATIC" or source_system == "FINOB":
+            sep = {"LRD_STATIC": "|", "FINOB": ","}.get(source_system, ",")
+            mock_read.csv.assert_any_call(
+                f"{test_container}/{source_system}/{file_name_base}",
+                sep=sep,
+                header=True,
+            )
+        else:
+            mock_read.parquet.assert_any_call(
+                f"{test_container}/{source_system}/{file_name_base}",
+            )
+        mock_write.assert_any_call(
+            f"{month_container}/sourcing_landing_data/NON_SSF/{source_system}/{file_name_no_ext}.parquet",
+            mode="overwrite",
+        )
+
+        assert extraction.move_source_file(**file)
+
+        calls = mock_dbutils_fs_mv.call_args_list
+        assert any(
+            args[0] == f"{test_container}/{source_system}/{file_name_base}"
+            and re.match(
+                f"{test_container}/{source_system}/processed/{file_name_no_ext}__\d{{8}}\d{{6}}{file_name_ext}",  # noqa: W605
+                args[1],
+            )
+            for args, _ in calls
+        )
+
+        data = extraction.extract_from_parquet(file["source_system"], file["file_name"])
+        stg_table_name = extraction.get_staging_table_name(file["file_name"])
+        assert extraction.save_to_stg_table(
+            data=data,
+            stg_table_name=stg_table_name,
+            **file,
+        )
+        mock_save_table.assert_any_call(
+            f"bsrc_d.stg_{run_month}.{file_name_no_ext.lower()}"
+        )
+
+        assert extraction.validate_data_quality(
+            **file,
+            stg_table_name=file_name_no_ext.lower(),
+        )
+
+    # Count update metadata and log calls
+    metadata_path_calls, log_path_calls = (
+        len(
+            [
+                call_args
+                for call_args in mock_save_table.call_args_list
+                if call_args == call(path)
+            ]
+        )
+        for path in [metadata_path, log_path]
+    )
+
+    # For every file (v1 - v4) we log every step from received + 1 - 5: (4 x 6)
+    # + 2 steps for v3.wrong_extension (received and initial checks)
+    # So in total 26 calls for metadata and log
+    assert metadata_path_calls == 26
+    assert log_path_calls == 26
