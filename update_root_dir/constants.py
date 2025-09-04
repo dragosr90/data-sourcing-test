f390abcedb0702427d920d173d312bf3846aacfc
dragosr90 <dragos.raduta@yahoo.com>
2025-09-04 14:56:22 +0300

    Create constants.py

---
+++ update_root_dir/constants.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+PROJECT_ROOT_DIRECTORY = Path(__file__).parent.parent.parent.parent.resolve()
+# Default value to be changed to location of deployed mapping / metadata files
